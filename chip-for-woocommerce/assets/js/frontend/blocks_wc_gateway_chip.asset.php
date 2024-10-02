8faf6d215f20a34acc868bd1d5cec72a159bf779
Sean Murphy <sean@iamseanmurphy.com>
2024-10-02 02:46:32 +0000

    1.6.4

---
+++ chip-for-woocommerce/assets/js/frontend/blocks_wc_gateway_chip.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-components', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => 'c017907e87c32fa266b2');
---
+++ chip-for-woocommerce/assets/js/frontend/blocks_wc_gateway_chip_2.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-components', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '20427fe4814998061f33');
